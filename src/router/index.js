e4ecb991450ee3c9fcc21fa68fdaaae7fdb83759
Le Thi Huyen Tran B2014710 <tranb2014710@student.ctu.edu.vn>
2024-01-09 01:20:54 +0700

    Tao trang them lien he

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,37 @@
+import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
-import { createRouter, createWebHistory } from "vue-router";
+
 const routes = [
     {
         path: "/",
         name: "contactbook",
         component: ContactBook,
     },
+
     {
         path: "/:pathMatch(.*)*",
-        name: "notFound",
+        name: "notfound",
         component: () => import("@/views/NotFound.vue"),
     },
+
     {
         path: "/contacts/:id",
         name: "contact.edit",
         component: () => import("@/views/ContactEdit.vue"),
         props: true
     },
+
+    {
+        path: "/contacts/",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
+        props: true
+    },
 ];
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
 });
+
 export default router;
